67e255b68f729528e40f267aa7033d79b7e919e6
Gabor Kozma <gabo@kozmagabor.com>
2018-03-19 14:16:56 +0100

    Remove node_exporter install, upgrade applications versions

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,9 +3,14 @@
 set -o nounset
 set -o pipefail
 set -o errexit
- 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+
+export DEBIAN_FRONTEND=noninteractive
+
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" > /etc/apt/sources.list.d/kubernetes.list
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+echo "deb https://download.docker.com/linux/ubuntu xenial stable" > /etc/apt/sources.list.d/docker-ce.list
 
 apt-get update -y
 apt-get install -y \
@@ -15,10 +20,10 @@ apt-get install -y \
     cloud-utils \
     cloud-init \
     cloud-initramfs-growroot \
-    docker.io=1.13.1-0ubuntu1~16.04.2 \
-    kubelet=1.8.3-00 \
-    kubeadm=1.8.3-00 \
-    kubernetes-cni=0.5.1-00 \
+    docker-ce=17.12.0~ce-0~ubuntu \
+    kubelet="${KUBERNETES_VERSION}-00" \
+    kubeadm="${KUBERNETES_VERSION}-00" \
+    kubernetes-cni=0.6.0-00 \
     sysstat \
     iotop \
     rsync \
@@ -29,19 +34,12 @@ apt-get install -y \
     jq
 
 # We don't want to upgrade them.
-apt-mark hold kubeadm kubectl kubelet kubernetes-cni
-
-apt-get upgrade -y
-pip install --upgrade pip
+apt-mark hold kubeadm kubectl kubelet kubernetes-cni docker-ce
 
 systemctl enable docker
 systemctl start docker
 
-sudo pip install awscli
-
-login="$(aws ecr get-login)"
-
-${login}
+apt-get -o Dpkg::Options::="--force-confold" upgrade -q -y --force-yes 
 
 # Check this list. :)
 # kubectl get pods --all-namespaces -o jsonpath="{..image}" |\
@@ -50,7 +48,15 @@ ${login}
 # uniq -c
 
 images=(
-  "gcr.io/google_containers/defaultbackend:1.3"
+  "banzaicloud/spark-driver:${SPARK_RELEASE_TAG}"
+  "banzaicloud/spark-driver-py:${SPARK_RELEASE_TAG}"
+  "banzaicloud/spark-executor:${SPARK_RELEASE_TAG}"
+  "banzaicloud/spark-executor-py:${SPARK_RELEASE_TAG}"
+  "banzaicloud/spark-init:${SPARK_RELEASE_TAG}"
+  "banzaicloud/spark-resource-staging-server:${SPARK_RELEASE_TAG}"
+  "banzaicloud/spark-shuffle:${SPARK_RELEASE_TAG}"
+  "banzaicloud/zeppelin-server:${ZEPPELIN_RELEASE_TAG}"
+
   "gcr.io/google_containers/etcd-amd64:${ETCD_RELEASE_TAG}"
 
   "gcr.io/google_containers/k8s-dns-dnsmasq-nanny-amd64:${K8S_DNS_RELEASE_TAG}"
@@ -61,62 +67,20 @@ images=(
   "gcr.io/google_containers/kube-controller-manager-amd64:${KUBERNETES_RELEASE_TAG}"
   "gcr.io/google_containers/kube-proxy-amd64:${KUBERNETES_RELEASE_TAG}"
   "gcr.io/google_containers/kube-scheduler-amd64:${KUBERNETES_RELEASE_TAG}"
-  "gcr.io/google_containers/kube-state-metrics:v0.5.0"
+  "gcr.io/google_containers/kube-state-metrics:v1.2.0"
   "gcr.io/google_containers/pause-amd64:3.0"
   "gcr.io/kubernetes-helm/tiller:${HELM_RELEASE_TAG}"
 
+  "banzaicloud/pushgateway:${PUSHGATEWAY_RELEASE_TAG}"
+  "prom/prometheus:${PROMETHEUS_RELEASE_TAG}"
   "jimmidyson/configmap-reload:v0.1"
-  "nginx:1.8"
 
-  "banzaicloud/pushgateway:${PUSHGATEWAY_RELEASE_TAG:-v0.3.1}"
-  "prom/prometheus:${PROMETHEUS_RELEASE_TAG:-v1.7.1}"
-
-  "banzaicloud/spark-driver:${SPARK_RELEASE_TAG}"
-  "banzaicloud/spark-driver-py:${SPARK_RELEASE_TAG}"
-  "banzaicloud/spark-executor:${SPARK_RELEASE_TAG}"
-  "banzaicloud/spark-executor-py:${SPARK_RELEASE_TAG}"
-  "banzaicloud/spark-init:${SPARK_RELEASE_TAG}"
-  "banzaicloud/spark-resource-staging-server:${SPARK_RELEASE_TAG}"
-  "banzaicloud/spark-shuffle:${SPARK_RELEASE_TAG}"
-  "banzaicloud/zeppelin-server:${ZEPPELIN_RELEASE_TAG}"
-
+  "traefik:${TRAEFIK_RELEASE_TAG}"
   "weaveworks/weave-npc:${WEAVE_RELEASE_TAG}"
   "weaveworks/weave-kube:${WEAVE_RELEASE_TAG}"
 )
 
 for i in "${images[@]}" ; do docker pull "${i}" ; done
-
-apt-get upgrade -y
-
-#install helm
-curl https://storage.googleapis.com/kubernetes-helm/helm-${HELM_RELEASE_TAG}-linux-amd64.tar.gz | tar xz --strip 1 -C /usr/bin/
-
-# Helm Charts
-mkdir /opt/helm
-cd /opt/helm
-
-helm init -c
-helm repo add banzaicloud-stable http://kubernetes-charts.banzaicloud.com
-helm repo update
-helm repo list
-helm fetch banzaicloud-stable/pipeline-cluster
-helm fetch banzaicloud-stable/pipeline-cluster-ingress
-tar -xvzf pipeline-cluster*
-rm -rf /home/ubuntu/.helm
-
-cd -
-
-# Helm Charts end
-
-
-# Install prometheus node_exporter
-mkdir /opt/node_exporter
-wget -qO- "https://github.com/prometheus/node_exporter/releases/download/v0.15.1/node_exporter-0.15.1.linux-amd64.tar.gz" | tar -xzvf - --strip-components=1 -C /opt/node_exporter/
-
-cp /tmp/node_exporter.service /etc/systemd/system/node_exporter.service
-
-systemctl enable node_exporter
-systemctl start  node_exporter
 
 ## Cleanup packer SSH key and machine ID generated for this boot
 rm /root/.ssh/authorized_keys
